5c0c798f13472426961fbd76a0012855987559a0
Kwon Sung Wook[ 권성욱 ] <baw0803@linecorp.com>
2023-07-11 14:55:53 +0900

    Tuist Xcode Version Setting

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -1,6 +1,8 @@
 import ProjectDescription
 
 let config = Config(
+    compatibleXcodeVersions: .upToNextMinor("14.3.1"),
+    swiftVersion: "5.8.0",
     plugins: [
         .local(path: .relativeToManifest("../../Plugins/ToySwiftUI")),
     ]
