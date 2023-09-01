314f761026902ffb3e2316e29567ca44195feaff
Kwon Sung Wook[ 권성욱 ] <baw0803@linecorp.com>
2023-09-01 17:24:02 +0900

    Refactoring

--- ToySwiftUI/Sources/ToySwiftUIApp.swift
+++ ToySwiftUI/Sources/ToySwiftUIApp.swift
@@ -1,24 +1,25 @@
-import SwiftUI
 import FeatureSupport
+import SwiftUI
 #if Alpha || Release
 import Firebase
 #endif
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-#if Alpha || Release
-    FirebaseApp.configure()
-#endif
-    print("Welcome to ToyProject!")
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil)
+    -> Bool {
+        #if Alpha || Release
+        FirebaseApp.configure()
+        #endif
+        print("Welcome to ToyProject!")
+        return true
+    }
 }
 
 @main
 struct ToySwiftUIApp: App {
 
-    // register app delegate
+    // Register app delegate
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     let myEnvironment = MyEnvironmnet()
