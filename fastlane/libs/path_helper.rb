196874b3a2befb84bf2e48773843693b9a84857a
Kwon Sung Wook[ 권성욱 ] <baw0803@linecorp.com>
2023-09-19 16:08:58 +0900

    Revise Testplan

--- fastlane/libs/path_helper.rb
+++ fastlane/libs/path_helper.rb
@@ -4,6 +4,10 @@ module PathHelper
     def derived_data_path
       File.join(output_directory, "DerivedData")
     end
+
+    def test_reports_path
+      File.join(output_directory, "test-reports")
+    end
   
     def output_directory
       ENV['OUTPUT_DIR']
