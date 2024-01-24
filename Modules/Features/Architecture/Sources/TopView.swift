2665ab406b729d4a23aa61796346603b50a803cd
Kwon Sung Wook[ 권성욱 ] <baw0803@linecorp.com>
2024-01-24 17:07:46 +0900

    [CID-21] Add SwiftLint Rule

--- Modules/Features/Architecture/Sources/TopView.swift
+++ Modules/Features/Architecture/Sources/TopView.swift
@@ -1,11 +1,11 @@
 import SwiftUI
 
 public struct TopView: View {
-    var hi: String = "hi"
     public init() { }
     public var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    } }
+    } 
+}
 
 #Preview {
     TopView()
